ac91917fbd56965551107883a979d7cb9e959e15
mnguyen <mnguyen@evenica.com>
2024-03-04 01:09:44 -0500

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import _cors from 'cors'
 
 export function cors() {
-  const useDevHost = process.env.DEV_HOST !== 'true'
+  const useDevHost = process.env.DEV_HOST === 'true'
   const devOrigin = 'https://blank.page'
   const origin = /\.mobius.cloud$/
   return _cors({ credentials: true, methods: ['GET', 'POST'], origin: useDevHost ? [devOrigin, origin] : origin })
